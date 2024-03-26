c7667f6994c8cbfe70c508cb3e1ba17d4129c7e6
Mestik78 <mestik78@gmail.com>
2024-03-26 12:58:05 +0100

    Prev
    Desarrollo previo al uso de Git.

---
+++ scripts/loadTemplates.js
@@ -0,0 +1,21 @@
+var templates
+
+const loadTemplateLevel = async function() {
+    templates = document.getElementsByTagName("template");
+
+    for (let template of templates) {
+        await fetch("templates/" + template.id + '.htm')
+            .then(response => response.text())
+            .then(html => {
+                template.outerHTML = html
+            });
+    }
+}
+
+const loadTemplates = async function() {
+    do {
+        await loadTemplateLevel()
+    } while (templates.length > 0)
+}
+
+loadTemplates()
